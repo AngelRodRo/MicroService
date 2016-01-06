c4a98ef7868427d5820fe11e83db0c9f615f6554
Colaborador Afipe | pc-05 <aleangrodriguez123@gmail.com>
2016-01-06 13:16:46 -0500

    Initial Commit

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'vendor/autoload.php';
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+    public function upload()
+    {
+
+    }
+
+    public function uploadToBase64()
+    {
+
+    }
+
+    public function download()
+    {
+
+    }
+
+}
---
+++ app/controllers/StorageController.php
@@ -0,0 +1,16 @@
+<?php
+
+class StorageController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+    public function uploadImage()
+    {
+
+    }
+
+}
